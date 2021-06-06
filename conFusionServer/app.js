fa14b6c1e4b737c58f882cb1c8e5a2a2f2f87ca3
peter-vu <vutuananh.vn.sg@gmail.com>
2021-06-06 23:39:32 +0800

    Session 2

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -32,36 +32,18 @@ function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-      return;
-    }
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == 'admin' && pass == 'password') {
-      req.session.user = 'admin';
-      next(); // authorized
-    } else {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-    }
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
   }
   else {
-    if (req.session.user === 'admin') {
-      console.log('req.session: ', req.session);
+    if (req.session.user === 'authenticated') {
       next();
     }
     else {
       var err = new Error('You are not authenticated!');
-      err.status = 401;
-      next(err);
+      err.status = 403;
+      return next(err);
     }
   }
 }
@@ -77,6 +59,8 @@ app.use(session({
   resave: false,
   store: new FileStore()
 }));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.use(auth);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -88,8 +72,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter)
