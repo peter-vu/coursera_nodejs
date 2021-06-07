ac4314f7341231dcf81cd0ed8107542a110b2062
peter-vu <vutuananh.vn.sg@gmail.com>
2021-06-07 14:57:45 +0800

    JWT

--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -15,21 +15,33 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/signup', (req, res, next) => {
-  User.register(new User({ username: req.body.username }),
+  User.register(new User({username: req.body.username}), 
     req.body.password, (err, user) => {
-      if (err) {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({ err: err });
-      }
-      else {
+    if(err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({err: err});
+    }
+    else {
+      if (req.body.firstname)
+        user.firstname = req.body.firstname;
+      if (req.body.lastname)
+        user.lastname = req.body.lastname;
+      user.save((err, user) => {
+        if (err) {
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({err: err});
+          return ;
+        }
         passport.authenticate('local')(req, res, () => {
           res.statusCode = 200;
           res.setHeader('Content-Type', 'application/json');
-          res.json({ success: true, status: 'Registration Successful!' });
+          res.json({success: true, status: 'Registration Successful!'});
         });
-      }
-    });
+      });
+    }
+  });
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
@@ -49,7 +61,8 @@ router.get('/logout', (req, res) => {
   else {
     var err = new Error('You are not logged in!');
     err.status = 403;
-    next(err);
+    // next(err);
+    res.redirect('/');
   }
 });
 
