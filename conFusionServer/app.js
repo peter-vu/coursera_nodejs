7821157bac854563ae4c94ddbde4d49b468027a5
Vu Tuan Anh <vutuananh.vn.sg@gmail.com>
2021-06-11 16:39:34 +0800

    Uploading

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -11,6 +11,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
@@ -75,7 +76,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
-app.use('/leaders', leaderRouter)
+app.use('/leaders', leaderRouter);
+app.use('/imageUpload',uploadRouter);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
