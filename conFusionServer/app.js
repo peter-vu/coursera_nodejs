b56a4f8718fecf4e114761cd2ab8efbd042bbedf
Vu Tuan Anh <vutuananh.vn.sg@gmail.com>
2021-06-11 11:13:08 +0800

    SSL

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -92,4 +92,15 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
+
+
 module.exports = app;
