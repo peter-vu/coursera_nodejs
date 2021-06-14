abf25e8b585696b1919dba971169befcb44d98db
Vu Tuan Anh <vutuananh.vn.sg@gmail.com>
2021-06-14 11:38:38 +0800

    Facebook OAuth2

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '867959837484899',
+        clientSecret: 'Yafb91d71ab6ee9ceb39ad038cca63893'
+    }
 }
